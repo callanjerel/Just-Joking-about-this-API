e594f14bcbcc2a9103948874accf655ac9c2b9e3
callanjerel <333221.rob@gmail.com>
2023-08-08 18:47:35 -0600

    Now fully functional :)

--- jokedata.js
+++ jokedata.js
@@ -1,6 +1,16 @@
 const { MongoClient } = require('mongodb')
 const url = "mongodb://localhost:2717"
 
+/*
+ * Reference for joke dataset:
+ * Title: A dataset of English plaintext jokes.
+ * URL: https://github.com/taivop/joke-dataset
+ * Author: Taivo Pungas
+ * Year: 2017
+ * Publisher: GitHub
+ * Journal: GitHub repository
+ */
+
 const getMongoClient = async (query) => {
     const client = new MongoClient(url)
     try {
@@ -18,11 +28,50 @@ const GetJoke = async (callback) => {
     getMongoClient(async (client) => {
         const database = client.db('jokesdb')
         const coll = database.collection('jokes')
-        let jokeData = await coll.findOne();
-        callback(jokeData)
+        let aggCursor = await coll.aggregate([{$sample:{size:1}}])
+        for await (const doc of aggCursor) {
+            callback(doc)
+        }
+    })
+}
+
+const UpdateJoke = async (updatedJoke, callback) => {
+    getMongoClient(async (client) => {
+        const database = client.db('jokesdb')
+        const coll = database.collection('jokes')
+        await coll.updateOne(
+            { id: updatedJoke.id },
+            { $set: { 
+                    title: updatedJoke.title, 
+                    category: updatedJoke.category,
+                    body: updatedJoke.body
+                }
+            })
+        callback()
+    })
+}
+
+const InsertJoke = async (newJoke, callback) => {
+    getMongoClient(async (client) => {
+        const database = client.db('jokesdb')
+        const coll = database.collection('jokes')
+        await coll.insertOne(newJoke)
+        callback()
+    })
+}
+
+const RemoveJoke = async (jokeId, callback) => {
+    getMongoClient(async (client) => {
+        const database = client.db('jokesdb')
+        const coll = database.collection('jokes')
+        await coll.deleteOne({ id: jokeId })
+        callback()
     })
 }
 
 module.exports = {
-    Get: GetJoke
+    Get: GetJoke,
+    Update: UpdateJoke,
+    Insert: InsertJoke,
+    Remove: RemoveJoke
 }
--- routes/jokes.js
+++ routes/jokes.js
@@ -3,13 +3,49 @@ const dal = require('../jokedata.js')
 const router = express.Router()
 
 const get = (req, res) => {
-    res.json({
-        'joke': true,
-        'punchline': 'your mother'
-    })
+    try {
+        dal.Get((joke) => {
+            res.json(joke)
+        })
+    } catch(err) {
+        res.sendStatus(500)
+    }
+}
+
+const patch = (req, res) => {
+    try {
+        dal.Update(req.body, () => {
+            res.sendStatus(200)
+        })
+    } catch(err) {
+        res.sendStatus(500)
+    }
+}
+
+const post = (req, res) => {
+    try {
+        dal.Insert(req.body, () => {
+            res.sendStatus(200)
+        })
+    } catch(err) {
+        res.sendStatus(500)
+    }
+}
+
+const remove = (req, res) => {
+    try {
+        dal.Remove(req.body.id, () => {
+            res.sendStatus(200)
+        })
+    } catch(err) {
+        res.sendStatus(500)
+    }
 }
 
 router.get('/', get)
-// router.patch('/', patch)
+router.patch('/', patch)
+router.put('/', patch)
+router.post('/', post)
+router.delete('/', remove)
 
 module.exports = router
